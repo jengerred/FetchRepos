fbca6c7a329b297078f35d7ec28481c33049b023
Jennifer <56004257+jengerred@users.noreply.github.com>
2023-01-26 16:44:40 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ fetch(' https://api.github.com/users/jengerred/repos')
 })
 .then(data => {
  data.forEach(repo => {
-     const markup = `<div class="box"><li class="name"><h3>${repo.name}</h1></li><li class="des"><p>${repo.description}</p></li> <li class="url"><a href="${repo.html_url}" target="_blank"><img src="icons8-github-100.png" height="50px" width ="50px"></a><figcaption>Github Repository</figcaption></li></div>` ;
+   
+     const markup = `
+<div class="box">
+
+<li class="name"><h3>${repo.name}</h3></li>
+
+<li class="des"><p>${repo.description}</p></li>
+<br>
+
+<li class="url">
+<a href="https://jengerred.github.io/${repo.name}/" target="_blank">View Live Project Here (if applicaple)
+</a></li>
+<br>
+
+ <li class="url"><a href="${repo.html_url}" target="_blank"><img src="icons8-github-100.png" height="50px" width ="50px"></a><figcaption>Github Repository</figcaption></li></div>
+</div>`
+
      document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
  });
 })
