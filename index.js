82bb86159c5b8e8112b301f7b0fcd91a27f68682
Jennifer <56004257+jengerred@users.noreply.github.com>
2023-01-26 18:39:23 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,17 +7,13 @@ fetch(' https://api.github.com/users/jengerred/repos')
    
      const markup = `
 <div class="box">
-
 <li class="name"><h3>${repo.name}</h3></li>
-
 <li class="des"><p>${repo.description}</p></li>
 <br>
-
 <li class="url">
 <a href="https://jengerred.github.io/${repo.name}/" target="_blank">View Live Project Here (if applicaple)
 </a></li>
 <br>
-
  <li class="url"><a href="${repo.html_url}" target="_blank"><img src="icons8-github-100.png" height="50px" width ="50px"></a><figcaption>Github Repository</figcaption></li></div>
 </div>`
 
