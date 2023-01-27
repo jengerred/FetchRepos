b2dabedc667848adc4e65e484f318aec7cf837ff
Jennifer <56004257+jengerred@users.noreply.github.com>
2023-01-27 17:07:20 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ fetch(' https://api.github.com/users/jengerred/repos')
    
      const markup = `
 <div class="box">
-<li class="name"><h3>${repo.name}</h3></li>
+<li class="name"><h3>${repo.name}</h3></li><hr><br>
 <li class="des"><p>${repo.description}</p></li>
-<br>
+
 <li class="url">
-<a href="https://jengerred.github.io/${repo.name}/" target="_blank">View Live Project Here (if applicaple)
+<a href="https://jengerred.github.io/${repo.name}/" target="_blank">View Live Project Here (if applicable)
 </a></li>
-<br>
- <li class="url"><a href="${repo.html_url}" target="_blank"><img src="icons8-github-100.png" height="50px" width ="50px"></a><figcaption>Github Repository</figcaption></li></div>
+
+ <li class="url"><a href="${repo.html_url}" target="_blank"><img src="icons8-github-100.png" height="50px" width ="50px"></a><figcaption>Github Repository</figcaption></li><br></div>
 </div>`
 
      document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
